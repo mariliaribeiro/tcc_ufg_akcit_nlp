fdf8aea2e5f3e7b75b8a3b2c4a69a7c0d7a1cd60
Marilia Ribeiro da Silva <marilia.ribeirods@gmail.com>
2025-06-05 15:21:11 -0300

    feat(connection): Update docstring

--- src/connetion/graph_db.py
+++ src/connetion/graph_db.py
@@ -8,7 +8,7 @@ from src.config import NEO4J_PASSWORD, NEO4J_URI, NEO4J_USERNAME
 @dataclass
 class KgDatabaseConnetion:
     """
-    Classe responsável pela connexão com o banco de dados que armazena o grafo de conhecimento.
+    Classe responsável pela conexão com o banco de dados que armazena o grafo de conhecimento.
     """
 
     graph_conn: Neo4jGraph = field(init=False, default=None)
@@ -17,6 +17,9 @@ class KgDatabaseConnetion:
         self.connection()
 
     def connection(self):
+        """
+        Estabelece a conexão com o Neo4J
+        """
         self.graph_conn = Neo4jGraph(
             url=NEO4J_URI,
             username=NEO4J_USERNAME,
