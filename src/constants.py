bd3e809a857604448ecf66c3f39e6838b806e896
Marilia Ribeiro da Silva <marilia.ribeirods@gmail.com>
2025-06-16 22:10:55 -0300

    feat: Graph modeling

---
+++ src/constants.py
@@ -0,0 +1,41 @@
+ALLOWED_NODES = [
+    "Empresa",
+    "Medicamento",
+    "Fármaco",
+    "Doença",
+    "Paciente",
+    "Profissão",
+    "Consumo",
+    "Frequência",
+]
+
+ALLOWED_RELATIONSHIPS = [
+    "FORNECE",
+    "INTERAÇÕES",
+    "PERTENCE",
+    "SEMELHANTE",
+    "INDICAÇÕES",
+    "COMPOSIÇÃO",
+    "EFICÁCIA",
+    "CONTRAINDICAÇÕES",
+    "INTERAÇÕES MEDICAMENTOSAS",
+    "ADVERTÊNCIAS E PRECAUÇÕES",
+    "REAÇÕES ADVERSAS",
+    "ADMINISTRAÇÃO",
+    "REGISTRO",
+    "PRODUZIDO",
+]
+
+NODE_PROPERTIES = [
+    "nome",
+    "quantidade",
+    "temperatura",
+    "validade",
+    "dose",
+    "percentual",
+    "duração",
+    "endereço",
+    "cnpj",
+    "contato",
+    "registro",
+]
